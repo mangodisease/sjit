0e58bc89aa49dec507f0f43753e90d09be820c8c
mangodisease <123826700+mangodisease@users.noreply.github.com>
2023-02-03 04:30:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,9 +33,8 @@ def test():
 async def encode(
     file: UploadFile = File(...)
 ):
-    raw_img = await file.read()
-    print(raw_img)
-    image = face_recognition.load_image_file(raw_img)
+    print(file)
+    image = face_recognition.load_image_file(file)
     encoded = face_recognition.face_encodings(image)[0]
 
     return {
